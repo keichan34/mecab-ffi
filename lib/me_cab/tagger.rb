fcace6e52871cbc185801e87a175776e5f215f71
Keitaroh Kobayashi <keita@kkob.us>
2014-08-04 16:01:30 +0900

    Don't crash the app if we're working with a null pointer

--- lib/me_cab/tagger.rb
+++ lib/me_cab/tagger.rb
@@ -1,6 +1,8 @@
 class MeCab::Tagger
   def initialize
     @mecab = MeCabLib.mecab_new2 ''
+    fail 'MeCab failed to initialize. Try running `mecab` on the command line, to see if you get any error message.' if @mecab.null?
+
     model = MeCabLib.mecab_model_new 0, ''
     @underlying_tagger = MeCabLib.mecab_model_new_tagger model
   end
